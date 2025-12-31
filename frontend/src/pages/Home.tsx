acf99065e526c13022554ff0f09046cc622c642d
vrajgfuturetech-spec <vraj.gfuturetech@gmail.com>
2025-12-31 12:50:53 +0530

    style: darken newsletter background to bg-maroon-100 for better balance

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -226,7 +226,7 @@ export default function Home() {
             </section>
 
             {/* Stay Updated Section */}
-            <section className="py-16 bg-maroon-50 text-maroon-900">
+            <section className="py-16 bg-maroon-100 text-maroon-900">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
                     <motion.div
                         initial={{ opacity: 0, y: 20 }}
