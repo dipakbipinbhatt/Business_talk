6cabc72e9ebe084f98a5f18715d1a8d3956faada
vrajgfuturetech-spec <vraj.gfuturetech@gmail.com>
2026-01-30 14:02:32 +0530

    Calender fix

--- frontend/src/pages/Admin/AdminCalendar.tsx
+++ frontend/src/pages/Admin/AdminCalendar.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { motion } from 'framer-motion';
+
 import logoImage from '../../assets/logo.jpg';
 import {
     ChevronLeft,
